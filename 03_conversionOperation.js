20f9f793e50b817ade99bfa5b784d4918ed0abc8
SYsrishtiyadav <152987037+SYsrishtiyadav@users.noreply.github.com>
2025-12-17 08:39:14 +0530

    Add files via upload

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,60 +1,59 @@
-let score = "33abc"
+let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
-//console.log (valueInNumber);
+//console.log(valueInNumber);
 
 // "33" => 33
-// "33sbc" => NaN
-// true => 1 , false => 0
+// "33abc" => NaN
+// true => 1; false => 0
 
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
 
-// 1 => true , 0 => false
-// "" => false , "abc" => true
+// 1 => true; 0 => false
+// "" => false
 // "srishti" => true
-// null => false
-// undefined => false
 
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
 
-// ********** Operations **********
+// ************* Operations **************
+
 let value = 3
 let negValue = -value
 //console.log(negValue);
 
-// console.log(3+3);
-// console.log(3-3);
-// console.log(3*3);
-// console.log(3/3);
-// console.log(3**3);
-// console.log(2%3);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
 
 let str1 = "hello"
-let str2 = "srishti"
+let str2 = " srishti"
 
 let str3 = str1 + str2
-console.log(str3);
+//console.log(str3);
 
 //console.log("1" + 2);
 //console.log(1 + "2");
 //console.log("1" + 2 + 2);
-//console.log(2 + 2 + "1");
+//console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
+//console.log( (3 + 4) * 5 % 3);
 
-console.log(+true);
-console.log(+"");
+//console.log(true);
+//console.log(+"");
 
 let num1, num2, num3
 
---
+++ synchronous.js
@@ -0,0 +1,10 @@
+// Javascript - 1.Synchronous Programming language hai
+//            - 2.single threaded
+
+// Execution Context - execute one line of code at a time
+//                   - console log-1
+//                   - console log-2   (each operation waits for the last one to complete before executing)
+
+// Blocking Code vs Non blocking code (two types ke code likhe jate hai Javascript me blocking code aur non blocking code)
+// Blocking code - Block the flow of program(Read File Sync)
+// Non blocking code - Does not block execution(Read File Async)
